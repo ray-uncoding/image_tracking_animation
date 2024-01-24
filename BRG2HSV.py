f0b24e64bf7541e94649e4bb0e4ec06994f0e0aa
Linsheng Pan <141114823+ray-uncoding@users.noreply.github.com>
2024-01-24 21:06:45 +0800

    workingONprogress

--- BRG2HSV.py
+++ BRG2HSV.py
@@ -1,14 +1,16 @@
+# 如果需要做顏色辨識, 可以藉由這段程式碼找到顏色域值
+
 import cv2
 import numpy as np
-
 
 def empty(v):
     pass
 
+#img = cv2.imread('img_files/boki.jpg')  # 取得圖片
+#img = cv2.resize(img, (0, 0), fx=2.0, fy=2.0)  # 縮放
+#HSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-img = cv2.imread('img_files/boki.jpg')  # 取得圖片
-img = cv2.resize(img, (0, 0), fx=2.0, fy=2.0)
-HSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+cap = cv2.VideoCapture(1)
 
 # 使用 cv2.WINDOW_NORMAL 以允許視窗大小的調整
 cv2.namedWindow('TrackBar', cv2.WINDOW_NORMAL)
@@ -22,6 +24,9 @@ cv2.createTrackbar("Val Min", "TrackBar", 0, 255, empty)
 cv2.createTrackbar("Val Max", "TrackBar", 255, 255, empty)
 
 while True:
+    ret, img = cap.read()
+    HSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    
     h_min = cv2.getTrackbarPos("Hue Min", "TrackBar")
     h_max = cv2.getTrackbarPos("Hue Max", "TrackBar")
     s_min = cv2.getTrackbarPos("Sat Min", "TrackBar")
@@ -37,7 +42,7 @@ while True:
     result = cv2.bitwise_and(img, img, mask=mask)
     cv2.waitKey(1)
 
-    cv2.imshow('img_boki', img)
-    cv2.imshow('HSV', HSV)
+    #cv2.imshow('img_boki', img)
+    #cv2.imshow('HSV', HSV)
     cv2.imshow('mask', mask)
     cv2.imshow('result', result)
---
+++ find_contours.py
@@ -0,0 +1,24 @@
+import cv2
+
+img = cv2.imread('img_files/boki.jpg')  # 讀圖
+img = cv2.resize(img, (0, 0), fx=2.0, fy=2.0)
+imgcontours = img.copy()
+img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # 轉灰階
+canny = cv2.Canny(img, 20, 400)  # 設定邊緣域值
+contours, hierarchy = cv2.findContours(canny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)  # 找邊緣
+
+for cnt in contours:
+    cv2.drawContours(imgcontours, cnt, -1, (255, 0, 0), 4)  # 在複製的影像中畫出輪廓
+    peri = cv2.arcLength(cnt, True)  # 計算閉合輪廓的邊長
+    vertices = cv2.approxPolyDP(cnt, peri * 0.02, True) #計算近似的多邊形, 輸出頂點
+    corners = len(vertices) #計算頂點數
+    x, y, w, h = cv2.boundingRect(vertices) #把所有頂點位置以方形框選時的大小
+    cv2.rectangle(imgcontours, (x,y), (x+w, y+h), (0, 255, 0), 4)
+
+cv2.imshow('img_boki', img)
+cv2.imshow('canny', canny)
+cv2.imshow('imgcontours', imgcontours)
+
+cv2.waitKey(0)
+
+
